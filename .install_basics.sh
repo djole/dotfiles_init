4c8acaefe9d122603a508f14af707f37b332ccce
Djordje Grbic <djgr@itu.dk>
2023-05-03 15:13:55 +0200

    add pyenv install

--- .install_basics.sh
+++ .install_basics.sh
@@ -1,6 +1,8 @@
 # install zsh
 sudo apt install zsh
 chsh -s $(which zsh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+source .zshrc
 
 # install tmux packet manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

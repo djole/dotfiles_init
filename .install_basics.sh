03495ac5d0aadb0699c1ea1d8e8b239c516d66c8
Djordje Grbic <djgr@itu.dk>
2023-05-03 15:06:18 +0200

    add pyenv install

--- .install_basics.sh
+++ .install_basics.sh
@@ -7,3 +7,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install and configure neovim
 sudo apt install neovim
+
+# install pyenv
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+curl https://pyenv.run | zsh
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"

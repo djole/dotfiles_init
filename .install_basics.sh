264e54dade84018070f3c7e291915419b3983c5d
Djordje Grbic <djgr@itu.dk>
2023-05-03 14:34:25 +0200

    Add .zshrc. .tmux_conf and install basics script

---
+++ .install_basics.sh
@@ -0,0 +1,9 @@
+# install zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# install tmux packet manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install and configure neovim
+sudo apt install neovim
